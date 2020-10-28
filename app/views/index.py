23b0a8b8cba61cf741d1ae2a0409a4c4d39f169c
Mario Yankov <mario.iankov@gmail.com>
2020-10-28 23:06:42 +0200

    Initial commit

---
+++ app/views/index.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from app.forms.profiles import ProfileForm
+from app.models import Profile
+
+
+def index(request):
+    if Profile.objects.exists():
+        pass
+    else:
+        context = {
+            'form': ProfileForm(),
+        }
+
+        return render(request, 'home-no-profile.html', context)
---
+++ app/views/profiles.py
@@ -0,0 +1,14 @@
+def profile_index(request):
+    pass
+
+
+def create_profile(request):
+    pass
+
+
+def edit_profile(request):
+    pass
+
+
+def delete_profile(request):
+    pass
